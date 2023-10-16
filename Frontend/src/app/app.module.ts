1fd02ffe4e3145e0bd4bad56861c9c21c094a8fa
DevanshAgarwal7 <devaagarwal914@gmail.com>
2023-10-17 00:04:15 +0530

    Creating pages,compontents,guard,service

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { NotesComponent } from './components/notes/notes.component';
+import { NotesTemplateComponent } from './components/notes-template/notes-template.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    DashboardComponent,
+    ProfileComponent,
+    NotesComponent,
+    NotesTemplateComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Frontend/src/app/components/notes-template/notes-template.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notes-template',
+  templateUrl: './notes-template.component.html',
+  styleUrls: ['./notes-template.component.css']
+})
+export class NotesTemplateComponent {
+
+}
